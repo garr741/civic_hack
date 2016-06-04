c413be51c0cef08a0098e392e98b8850ed9085c5
Tylor Garrett <tylorgarrett@gmail.com>
2016-06-04 10:28:04 -0400

    initial commit

---
+++ app/src/main/java/xyz/tgprojects/seamless/MainActivity.java
@@ -0,0 +1,27 @@
+package xyz.tgprojects.seamless;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(this);
+
+    }
+
+    @Override public void onClick(View v) {
+        if ( v.getId() == R.id.login_button ){
+            Intent i = new Intent();
+            startActivity(i);
+        }
+    }
+}
