6f84a0965a1c62646c52a24ef4c3b9d13c7613c5
Tylor Garrett <tylorgarrett@gmail.com>
2016-06-04 13:52:58 -0400

    v0.1

--- app/src/main/java/xyz/tgprojects/seamless/CurrentRidersActivity.java
+++ app/src/main/java/xyz/tgprojects/seamless/CurrentRidersActivity.java
@@ -1,10 +1,16 @@
 package xyz.tgprojects.seamless;
 
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import xyz.tgprojects.seamless.adapters.RidersAdapter;
 
 public class CurrentRidersActivity extends AppCompatActivity {
@@ -12,6 +18,10 @@ public class CurrentRidersActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RidersAdapter adapter;
+
+    FloatingActionButton fab;
+
+    Toolbar toolbar;
 
 
     @Override protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +33,31 @@ public class CurrentRidersActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new RidersAdapter();
         recyclerView.setAdapter(adapter);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Current Rides");
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+
+            }
+        });
     }
 
-    @Override protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.riders_menu, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if ( id == R.id.menu_profile ){
+            startActivity(new Intent(this, ProfileActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
