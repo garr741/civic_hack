83500641a694c009181b8163051994b094547a93
Tylor Garrett <tylorgarrett@gmail.com>
2016-06-04 17:25:37 -0400

    sdf

--- app/src/main/java/xyz/tgprojects/seamless/ProfileActivity.java
+++ app/src/main/java/xyz/tgprojects/seamless/ProfileActivity.java
@@ -1,13 +1,27 @@
 package xyz.tgprojects.seamless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button button = (Button) findViewById(R.id.profile_buddy_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CurrentRidersActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
+
     }
 
 
--- app/src/main/java/xyz/tgprojects/seamless/adapters/RidersAdapter.java
+++ app/src/main/java/xyz/tgprojects/seamless/adapters/RidersAdapter.java
@@ -1,40 +1,48 @@
 package xyz.tgprojects.seamless.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import xyz.tgprojects.seamless.R;
+import xyz.tgprojects.seamless.RideActivity;
 
-public class RidersAdapter extends RecyclerView.Adapter<RidersAdapter.ViewHolder> {
+public class RidersAdapter extends RecyclerView.Adapter<RidersAdapter.ViewHolder> implements View.OnClickListener{
     private String[] mDataset;
     private String[] dist;
+    private Context context;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder{
         // each data item is just a string in this case
         public TextView mTextView;
         public TextView distanceView;
+        public LinearLayout layout;
         public ViewHolder(View v) {
             super(v);
-
+            layout = (LinearLayout) v.findViewById(R.id.rider_card_layout);
             mTextView = (TextView) v.findViewById(R.id.card_name);
             distanceView = (TextView) v.findViewById(R.id.card_description);
         }
+
+
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
     public RidersAdapter(Context c, String[]myDataset, String[] dist) {
         Log.d("ddd",dist.toString());
+        this.context = c;
         mDataset = myDataset;
         this.dist = dist;
     }
@@ -59,6 +67,7 @@ public class RidersAdapter extends RecyclerView.Adapter<RidersAdapter.ViewHolder
         // - replace the contents of the view with that element
         holder.mTextView.setText(mDataset[position]);
         holder.distanceView.setText(dist[position]);
+        holder.layout.setOnClickListener(this);
 
     }
 
@@ -66,6 +75,12 @@ public class RidersAdapter extends RecyclerView.Adapter<RidersAdapter.ViewHolder
     @Override
     public int getItemCount() {
         return mDataset.length;
+    }
+
+    @Override public void onClick(View v) {
+        Intent i = new Intent(context, RideActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
 }
 
