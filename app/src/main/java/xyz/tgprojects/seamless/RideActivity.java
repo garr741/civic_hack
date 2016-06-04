102ee3b5246c6593a6b8059b7104695f4836dbe4
Tylor Garrett <tylorgarrett@gmail.com>
2016-06-04 16:46:32 -0400

    joined ride

--- app/src/main/java/xyz/tgprojects/seamless/RideActivity.java
+++ app/src/main/java/xyz/tgprojects/seamless/RideActivity.java
@@ -1,5 +1,6 @@
 package xyz.tgprojects.seamless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -7,6 +8,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import xyz.tgprojects.seamless.adapters.RideAdapter;
 
 public class RideActivity extends AppCompatActivity {
@@ -14,7 +17,7 @@ public class RideActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Toolbar toolbar;
 
-
+    Button button;
 
 
     @Override protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,14 @@ public class RideActivity extends AppCompatActivity {
 
 
         recyclerView.setAdapter(rideAdapter);
+
+        button = (Button) findViewById(R.id.join_ride_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), JoinedRideActivity.class));
+            }
+        });
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
